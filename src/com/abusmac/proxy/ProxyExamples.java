e693324bbd3a289916186f61f8dc60b365a75ada
xmight <xmight>
2023-03-11 19:56:35 +0200

    Added implementing class for InvocationHandler

--- src/com/abusmac/proxy/ProxyExamples.java
+++ src/com/abusmac/proxy/ProxyExamples.java
@@ -1,6 +1,7 @@
 package com.abusmac.proxy;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class ProxyExamples implements Runnable {
@@ -15,7 +16,7 @@ public class ProxyExamples implements Runnable {
 
         // Invocation handler to trace the call to the ProxyExamples runFromWithinInvocationHandler method call.
         InvocationHandler invocationHandler = (proxy, method, args) -> {
-            System.out.println("Within invocation handler");
+            System.out.println("Within invocation handler from lambda");
 
             proxyExamples.runFromWithinInvocationHandler();
 
@@ -25,6 +26,26 @@ public class ProxyExamples implements Runnable {
         Runnable newProxyInstance = (Runnable) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{Runnable.class}, invocationHandler);
         newProxyInstance.run();
 
+        Runnable newProxyInstance2 = (Runnable) Proxy.newProxyInstance(this.getClass().getClassLoader(), new Class[]{Runnable.class}, new TraceInvocationHandler(this));
+        newProxyInstance2.run();
+
         System.out.println("End of ProxyExamples");
+    }
+
+    public class TraceInvocationHandler implements InvocationHandler {
+        final ProxyExamples proxyExamples;
+
+        public TraceInvocationHandler(ProxyExamples proxyExamples) {
+            this.proxyExamples = proxyExamples;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("Within invocation handler from implementing InvocationHandler class");
+
+            proxyExamples.runFromWithinInvocationHandler();
+
+            return null;
+        }
     }
 }
