00aae0d4e1e3f7b8c58cc64d4ef7ed2393176024
abusmac <abusmac>
2024-01-10 12:12:27 +0200

    Refactor ThreadingExamples.java

--- src/com/abusmac/threading/ThreadingExamples.java
+++ src/com/abusmac/threading/ThreadingExamples.java
@@ -1,15 +1,23 @@
 package com.abusmac.threading;
 
 public class ThreadingExamples implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        ThreadingExamples examples = new ThreadingExamples();
+        Thread t = new Thread(examples);
+
+        t.start();
+    }
+
     @Override
     public void run() {
-        Thread t = new Thread(new SomeRunnable(4000));
-        Thread t2 = new Thread(new SomeRunnable(7000));
+        Thread t = new Thread(new SomeRunnable(1000));
+        Thread t2 = new Thread(new SomeRunnable(2000));
 
         t.start();
         t2.start();
 
         try {
+            System.out.println("Before SomeRunnable 1: " + Thread.currentThread().getId() + "; " + System.currentTimeMillis());
             t.join();
             System.out.println("After SomeRunnable 1: " + Thread.currentThread().getId() + "; " + System.currentTimeMillis());
             t2.join();
@@ -19,5 +27,23 @@ public class ThreadingExamples implements Runnable {
         }
 
         System.out.println("End of ThreadingExamples: " + Thread.currentThread().getId() + "; " + System.currentTimeMillis());
+    }
+
+    private static class SomeRunnable implements Runnable {
+        int sleep;
+
+        public SomeRunnable(int sleep) {
+            this.sleep = sleep;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(sleep);
+                System.out.println("SomeRunnable " + Thread.currentThread().getId() + "; " + System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/com/abusmac/threading/ThreadingMain.java
+++
@@ -1,7 +0,0 @@
-package com.abusmac.threading;
-
-public class ThreadingMain {
-    public static void main(String[] args) throws InterruptedException {
-
-    }
-}
